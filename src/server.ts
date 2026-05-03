d3d6378764bc5bcb8ac3ab62c605fe529c267ad5
indranil <ibera866@gmail.com>
2026-05-03 18:44:58 +0530

    add frontend URL

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3000;
 
 
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "https://quiz-app-orpin-gamma.vercel.app",
     credentials: true
 }))
 console.log("request")
